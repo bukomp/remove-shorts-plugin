9266d98b3d154932f4d2a41d65d58d3ab44e9b31
Edvard <edvard@shalaev.com>
2023-03-05 19:34:35 +0300

    fix: changed post_build script to apply new version to the manifest.json after build

--- src/scripts/post_build.sh
+++ src/scripts/post_build.sh
@@ -1,2 +1,13 @@
+#!/bin/bash
+
+# Copy all the icons from the src/icons folder to the built folder
 cp -R src/icons/* ./built
+
+# Copy all the files from the dist folder to the built folder
 cp -R ./dist/* ./built
+
+# Get the version number from package.json
+VERSION=$(node -p "require('./package.json').version")
+
+# Replace the version number in manifest.json
+sed -i "s/\"version\": \".*\"/\"version\": \"$VERSION\"/" built/manifest.json
