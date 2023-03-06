cf27301cfbe4d75f5e6dc86424321d2b94f581d9
Edvard <edvard@shalaev.com>
2023-03-06 23:00:06 +0300

    feat: commented the code

--- src/pages/all.ts
+++ src/pages/all.ts
@@ -1,12 +1,20 @@
 import { waitUntilNodeLoads } from '../utils/utils';
 
+// variables for tracking if the Shorts buttons are removed or not
 let shortsButtonRemoved = false;
 let shortsMiniButtonRemoved = false;
 
-const removeShortsButton = () => {
+/**
+ * Remove the Shorts button from the navigation bar if it exists
+ */
+const removeShortsButton = (): void => {
+  // only remove the button if it hasn't been removed before
   if (!shortsButtonRemoved) {
+    // wait until the element is loaded with the specified selector, then remove it
     waitUntilNodeLoads('ytd-guide-entry-renderer.ytd-guide-section-renderer > a[id="endpoint"][title="Shorts"]', 50, 10).then((element) => {
+      // log the element for debugging purposes
       console.log(element);
+      // if the element exists, remove it and update the flag
       if (element) {
         shortsButtonRemoved = true;
         element?.remove();
@@ -15,10 +23,17 @@ const removeShortsButton = () => {
   }
 };
 
-const removeMiniShortsButton = () => {
+/**
+ * Remove the mini Shorts button from the navigation bar if it exists
+ */
+const removeMiniShortsButton = (): void => {
+  // only remove the button if it hasn't been removed before
   if (!shortsMiniButtonRemoved) {
+    // wait until the element is loaded with the specified selector, then remove it
     waitUntilNodeLoads('ytd-mini-guide-entry-renderer[aria-label="Shorts"] > a[id="endpoint"][title="Shorts"]', 50, 10).then((element) => {
+      // log the element for debugging purposes
       console.log(element);
+      // if the element exists, remove it and update the flag
       if (element) {
         shortsMiniButtonRemoved = true;
         element?.remove();
@@ -27,12 +42,18 @@ const removeMiniShortsButton = () => {
   }
 };
 
-const removeShortsButtons = async () => {
+/**
+ * Remove both the Shorts and mini Shorts buttons from the navigation bar, and add event listeners to handle when the guide button is clicked
+ */
+const removeShortsButtons = async (): Promise<void> => {
+  // remove both buttons
   removeShortsButton();
   removeMiniShortsButton();
 
+  // add event listeners to handle when the guide button is clicked
   document.querySelectorAll('yt-icon-button#guide-button > button#button').forEach((buttonElement) => {
     buttonElement.addEventListener('click', () => {
+      // when the guide button is clicked, remove both buttons again
       removeShortsButton();
       removeMiniShortsButton();
     });
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,15 +1,29 @@
+/**
+ * Sleep function that resolves after a specified number of milliseconds
+ * @param ms The number of milliseconds to wait
+ * @returns A promise that resolves after the specified number of milliseconds
+ */
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
+/**
+ * Waits until a node is loaded on the page and returns it
+ * @param nodeSelectors The CSS selector for the node to wait for
+ * @param timeout The number of milliseconds to wait before retrying
+ * @param loops The number of times to retry before giving up
+ * @returns A promise that resolves with the loaded element or null if not found
+ */
 const waitUntilNodeLoads = async (nodeSelectors: string, timeout = 100, loops = 6): Promise<Element | null> => {
   let contentsNode: Element | null = null;
 
-  await sleep(timeout);
+  await sleep(timeout); // Wait for a specified amount of time before trying to load the node
 
   let i = 0;
 
   while (contentsNode === null && i < loops) {
+    // Keep trying to find the node until it is loaded or the maximum number of loops is reached
     contentsNode = document.querySelector(nodeSelectors);
     if (contentsNode === null) {
+      // If the node is not found, wait for a specified amount of time before trying again
       await sleep(timeout * i);
       i++;
     }
@@ -18,17 +32,26 @@ const waitUntilNodeLoads = async (nodeSelectors: string, timeout = 100, loops = 
   return contentsNode;
 };
 
+/**
+ * Waits until multiple nodes are loaded on the page and returns them
+ * @param nodeSelectors The CSS selector for the nodes to wait for
+ * @param timeout The number of milliseconds to wait before retrying
+ * @param loops The number of times to retry before giving up
+ * @returns A promise that resolves with the loaded elements or an empty array if none found
+ */
 const waitUntilNodesLoad = async (nodeSelectors: string, timeout = 100, loops = 6): Promise<NodeListOf<Element> | []> => {
   let contentsNode: NodeListOf<Element> | [] = [];
 
-  await sleep(timeout);
+  await sleep(timeout); // Wait for a specified amount of time before trying to load the nodes
 
   let i = 0;
 
   while (contentsNode?.length === 0 && i < loops) {
+    // Keep trying to find the nodes until they are loaded or the maximum number of loops is reached
     contentsNode = document.querySelectorAll(nodeSelectors);
 
     if (contentsNode?.length === 0) {
+      // If no nodes are found, wait for a specified amount of time before trying again
       await sleep(timeout * i++);
     }
   }
@@ -36,22 +59,33 @@ const waitUntilNodesLoad = async (nodeSelectors: string, timeout = 100, loops = 
   return contentsNode;
 };
 
+/**
+ * Executes a callback function when the page changes
+ * @param callback The function to execute when the page changes
+ * @param initialExec Whether to execute the callback function on initial load
+ */
 const excecuteIfPageChangeTo = async (callback: (href: string) => Promise<void>, initialExec = false) => {
   if (initialExec) {
+    // If initialExec is set to true, execute the callback function immediately
     callback(document.location.href);
   }
 
-  let oldHref = document.location.href;
+  let oldHref = document.location.href; // Store the current page URL
   const body = document.querySelector('body') as HTMLBodyElement;
   const observer = new MutationObserver((mutations) => {
+    // Create a MutationObserver that listens for changes in the body element
     mutations.forEach(async (mutation) => {
+      // For each mutation, check if the URL has changed
       if (mutation.type !== 'childList' && oldHref !== document.location.href) {
+        // If the URL has changed, execute the callback function and update oldHref
         console.log(document.location.href);
         await callback(document.location.href);
         oldHref = document.location.href;
       }
     });
   });
+
+  // Start observing the body element for changes in URL
   observer.observe(body, { childList: true, subtree: true, attributes: true, attributeFilter: ['href'] });
 };
 
