11c60fcc4f44b30a3bac3608404765b1ff97e451
Edvard <edvard@shalaev.com>
2023-07-17 00:03:44 +0300

    fix: added manifest.json to the git

--- scripts/post_build.sh
+++ scripts/post_build.sh
@@ -6,8 +6,11 @@ cp -R ./icons/* ./build
 # Copy all the files from the dist folder to the built folder
 cp -R ./dist/* ./build
 
+
 # Get the version number from package.json
 VERSION=$(node -p "require('./package.json').version")
 
 # Replace the version number in manifest.json
-sed -i "s/\"version\": \".*\"/\"version\": \"$VERSION\"/" build/manifest.json
+sed -i "s/\"version\": \".*\"/\"version\": \"$VERSION\"/" ./manifest.json
+cp ./manifest.json ./build/manifest.json
+
