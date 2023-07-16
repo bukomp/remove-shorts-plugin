d65b10097acedccc0a65868253b0a5de7cd60481
Edvard <edvard@shalaev.com>
2023-07-17 00:17:45 +0300

    fix: minor fixes to bash script

--- scripts/post_build.sh
+++ scripts/post_build.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+buildDirectory="./build"
+
+#create a directory only if it doesn't exist
+if [ ! -d "$buildDirectory" ]; then
+    mkdir -p "$buildDirectory"
+fi
+
 # Copy all the icons from the src/icons folder to the built folder
-cp -R ./icons/* ./build
+cp -R ./icons/* $buildDirectory
 
 # Copy all the files from the dist folder to the built folder
-cp -R ./dist/* ./build
+cp -R ./dist/* $buildDirectory
 
 
 # Get the version number from package.json
@@ -12,5 +19,5 @@ VERSION=$(node -p "require('./package.json').version")
 
 # Replace the version number in manifest.json
 sed -i "s/\"version\": \".*\"/\"version\": \"$VERSION\"/" ./manifest.json
-cp ./manifest.json ./build/manifest.json
+cp ./manifest.json $buildDirectory/manifest.json
 
